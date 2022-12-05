d2ace6e105e1881d50a6054b7cdfcdb04c8cb331
tangky <945541696@qq.com>
2022-12-05 09:46:40 +0800

    first commit

---
+++ src/layouts/SecurityLayout.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+// {}:插值,让jsx解析器知道这个是js而非字符串
+import { PageLoading } from '@ant-design/pro-layout';
+import { Redirect, connect } from 'umi';
+import { stringify } from 'querystring';
+
+class SecurityLayout extends React.Component {
+  state = {
+    isReady: false,
+  };
+
+  componentDidMount() {
+    this.setState({
+      isReady: true,
+    });
+    const { dispatch } = this.props;
+
+    if (dispatch) {
+      dispatch({
+        type: 'user/fetchCurrent',
+      });
+    }
+  }
+
+  render() {
+    const { isReady } = this.state;
+    const { children, loading, currentUser } = this.props; // You can replace it to your authentication rule (such as check token exists)
+    // 你可以把它替换成你自己的登录认证规则（比如判断 token 是否存在）
+
+    // const isLogin = currentUser && currentUser.userid;
+    const isLogin = currentUser && currentUser.id;
+    const queryString = stringify({
+      // 刷新当前页面
+      redirect: window.location.href,
+    });
+
+    if ((!isLogin && loading) || !isReady) {
+      return <PageLoading />;
+    }
+
+    if (!isLogin && window.location.pathname !== '/user/login') {
+      return <Redirect to={`/user/login?${queryString}`} />;
+    }
+
+    return children;
+  }
+}
+// connect(数据)(组件)将model层的数据传给组件
+export default connect(({ user, loading }) => ({
+  currentUser: user.currentUser,
+  loading: loading.models.user,
+}))(SecurityLayout);
---
+++ src/models/login.js
@@ -0,0 +1,74 @@
+import { stringify } from 'querystring';
+import { history } from 'umi';
+// import { fakeAccountLogin } from '@/services/login';
+import { login } from '@/services/login';
+import { setAuthority } from '@/utils/authority';
+import { getPageQuery } from '@/utils/utils';
+import { message } from 'antd';
+import { CONFIG } from '@/consts/config';
+const Model = {
+  namespace: 'login', // redux的key
+  state: {
+    status: undefined,
+  },
+  effects: {
+    *login({ payload }, { call, put }) {
+      // const response = yield call(fakeAccountLogin, payload);
+      const response = yield call(login, payload); // call 调用外部异步方法
+      yield put({
+        //redux的dispatch
+        type: 'changeLoginStatus',
+        payload: response,
+      }); // Login successfully
+
+      if (response.code === 200) {
+        debugger;
+        const urlParams = new URL(window.location.href);
+        const params = getPageQuery();
+        message.success('🎉 🎉 🎉  登录成功！');
+        let { redirect } = params;
+
+        if (redirect) {
+          const redirectUrlParams = new URL(redirect);
+
+          if (redirectUrlParams.origin === urlParams.origin) {
+            redirect = redirect.substr(urlParams.origin.length);
+
+            if (redirect.match(/^\/.*#/)) {
+              redirect = redirect.substr(redirect.indexOf('#') + 1);
+            }
+          } else {
+            message.error(response.msg);
+          }
+        }
+
+        history.replace(redirect || '/');
+      }
+    },
+
+    logout() {
+      const { redirect } = getPageQuery(); // Note: There may be security issues, please note
+
+      if (window.location.pathname !== '/user/login' && !redirect) {
+        history.replace({
+          pathname: '/user/login',
+          search: stringify({
+            redirect: window.location.href,
+          }),
+        });
+      }
+    },
+  },
+  reducers: {
+    changeLoginStatus(state, { payload }) {
+      // 导入用户信息
+      localStorage.setItem('pitangToken', payload.data.token);
+      // localStorage操作浏览器缓存,但只接受string和string的键值对,故需将user对象序列化为string
+      localStorage.setItem('pitangUser', JSON.stringify(payload.data.user));
+      // setAuthority(payload.currentAuthority);
+      setAuthority(CONFIG.ROLE[payload.data.user.role]);
+      return { ...state, status: payload.code === 200 ? 'ok' : 'error', type: payload.type };
+    },
+  },
+};
+export default Model;
