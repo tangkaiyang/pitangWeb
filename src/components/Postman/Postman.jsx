7bf58d10d9329795b6a567fc7e826268f37ab0cb
大胖子 <945541696@qq.com>
2022-12-13 08:08:29 +0800

    ->antd版本调整

--- src/components/Postman/Postman.jsx
+++ src/components/Postman/Postman.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Card, Col, Row, Input, Select, Button } from 'antd';
+import { Card, Col, Row, Input, Select, Button, TabPane, Tabs } from 'antd';
 import { SendOutlined } from '@ant-design/icons';
-
+// Col把栅格分成24份
 const { Option } = Select;
 const selectBefore = (
-  <Select defaultValue="http://" className="select-before">
-    <Option value="http://">http://</Option>
-    <Option value="https://">https://</Option>
+  <Select defaultValue="GET" className="select-before">
+    <Option value="GET">GET</Option>
+    <Option value="POST">POST</Option>
   </Select>
 );
 export default () => {
@@ -18,9 +18,23 @@ export default () => {
         </Col>
         <Col span={6}>
           <Button type="primary" size="large" style={{ marginRight: 16, float: 'right' }}>
-            <SendOutlined/>Send
+            <SendOutlined />
+            Send
           </Button>
         </Col>
+      </Row>
+      <Row style={{ marginTop: 8 }}>
+        <Tabs defaultActiveKey="1">
+          <TabPane tab="Params" key="1">
+            这是Params
+          </TabPane>
+          <TabPane tab="Headers" key="2">
+            这是Headers
+          </TabPane>
+          <TabPane tab="Body" key="3">
+            这是Body
+          </TabPane>
+        </Tabs>
       </Row>
     </Card>
   );
