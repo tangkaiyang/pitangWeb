ba3f167c4dbe60ded5da8e3ed4ed94877d2ab5fd
大胖子 <945541696@qq.com>
2022-12-15 08:18:49 +0800

    json Dropdown

--- src/components/Postman/Postman.jsx
+++ src/components/Postman/Postman.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Card, Col, Row, Input, Select, Button, Tabs, Radio } from 'antd';
 import { SendOutlined } from '@ant-design/icons';
+import { DownOutlined, SmileOutlined } from '@ant-design/icons';
+import { Dropdown, Space } from 'antd';
+
 // Col把栅格分成24份
 const { Option } = Select;
 const selectBefore = (
@@ -9,6 +12,42 @@ const selectBefore = (
     <Option value="POST">POST</Option>
   </Select>
 );
+
+const items = [
+  {
+    key: '1',
+    label: (
+      <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+        1st menu item
+      </a>
+    ),
+  },
+  {
+    key: '2',
+    label: (
+      <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+        2nd menu item (disabled)
+      </a>
+    ),
+    icon: <SmileOutlined />,
+    disabled: true,
+  },
+  {
+    key: '3',
+    label: (
+      <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+        3rd menu item (disabled)
+      </a>
+    ),
+    disabled: true,
+  },
+  {
+    key: '4',
+    danger: true,
+    label: 'a danger item',
+  },
+];
+
 export default () => {
   // bodyType是变量名,setBodyType是改变bodyType的方法,none是bodyType默认值
   const [bodyType, setBodyType] = useState('none');
@@ -53,12 +92,24 @@ export default () => {
                   <Radio value="binary">binary</Radio>
                   <Radio value="GraphQL">GraphQL</Radio>
                 </Radio.Group>
-                // {bodyType === 'raw' ?
-                // }
               ),
             },
           ]}
         />
+        {bodyType === 'raw' ? (
+          <Dropdown
+            menu={{
+              items,
+            }}
+          >
+            <a onClick={(e) => e.preventDefault()}>
+              <Space>
+                Hover me
+                <DownOutlined />
+              </Space>
+            </a>
+          </Dropdown>
+        ) : null}
       </Row>
     </Card>
   );
