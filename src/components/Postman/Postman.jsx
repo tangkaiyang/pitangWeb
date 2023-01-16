931c5e227f21b34d54ca83fca57fd1732625a0f2
tangky <945541696@qq.com>
2023-01-16 14:50:18 +0800

    params编辑

--- src/components/Postman/Postman.jsx
+++ src/components/Postman/Postman.jsx
@@ -5,18 +5,64 @@ import { DownOutlined, SmileOutlined } from '@ant-design/icons';
 import { Dropdown, Space } from 'antd';
 import EditableTable from '@/components/Table/EditableTable';
 
+// Col把栅格分成24份
+const { Option } = Select;
+const selectBefore = (
+  <Select defaultValue="GET" className="select-before">
+    <Option value="GET">GET</Option>
+    <Option value="POST">POST</Option>
+  </Select>
+);
 export default () => {
-  // Col把栅格分成24份
-  const { Option } = Select;
-  const selectBefore = (
-    <Select defaultValue="GET" className="select-before">
-      <Option value="GET">GET</Option>
-      <Option value="POST">POST</Option>
-    </Select>
-  );
+  // bodyType是变量名,setBodyType是改变bodyType的方法,none是bodyType默认值
+  const [bodyType, setBodyType] = useState('none');
+  const [rawType, setRawType] = useState('JSON');
+  // useState(初始值而不是变量名)
+  const [paramsData, setParamsData] = useState([]);
+  const [editableKeys, setEditableRowKeys] = useState(() => {
+    paramsData.map((item) => item.id);
+  });
 
   const onClickMenu = (key) => {
     setRawType(key);
+  };
+
+  // 请求url+params
+  const [url, setUrl] = useState('');
+
+  // 根据paramsData拼接url
+  const joinUrl = (data) => {
+    let tempUrl = url.split('?')[0];
+    data.forEach((item, idx) => {
+      // 如果item.key有效
+      if (item.key) {
+        if (idx === 0) {
+          tempUrl = `${tempUrl}?${item.key}=${item.value || ''}}`;
+        } else {
+          tempUrl = `${tempUrl}&${item.key}=${item.value || ''}}`;
+        }
+      }
+    });
+    setUrl(tempUrl);
+  };
+
+  const splitUrl = (nowUrl) => {
+    const split = nowUrl.split('?');
+    if (split.length < 2) {
+      setParamsData([]);
+    } else {
+      const params = split[1].split('&');
+      const newParams = [];
+      const keys = [];
+      params.forEach((item, idx) => {
+        const [key, value] = item.split('=');
+        const now = Data.now();
+        keys.push(now + idx + 10);
+        newParams.push({ key, value, id: now + idx + 10, description: '' });
+      });
+      setParamsData(newParams);
+      setEditableRowKeys(keys);
+    }
   };
 
   const menu = (
@@ -68,28 +114,41 @@ export default () => {
       </Menu.Item>
     </Menu>
   );
-  // bodyType是变量名,setBodyType是改变bodyType的方法,none是bodyType默认值
-  const [bodyType, setBodyType] = useState('none');
-  const [rawType, setRawType] = useState('JSON');
-  const [paramsData, setParamsData] = useState('Params');
 
   const paramsColumns = [
-    { title: 'KEY', dataIndex: 'key' },
-    { title: 'VALUE', dataIndex: 'value' },
-    { title: 'DESCRIPTION', dataIndex: 'description' },
+    {
+      title: 'KEY',
+      dataIndex: 'key',
+    },
+    {
+      title: 'VALUE',
+      dataIndex: 'value',
+    },
+    {
+      title: 'DESCRIPTION',
+      dataIndex: 'description',
+    },
     {
       title: '操作',
-      render: (text, record) => {
-        return (
-          <DeleteTwoTone
-            style={{ cursor: 'pointer' }}
-            onClick={() => {
-              onDelete(record.id);
-            }}
-          />
-        );
+      valueType: 'option',
+      width: 250,
+      render: () => {
+        return null;
       },
     },
+    // {
+    //   title: '操作',
+    //   render: (text, record) => {
+    //     return (
+    //       <DeleteTwoTone
+    //         style={{ cursor: 'pointer' }}
+    //         onClick={() => {
+    //           onDelete(record.id);
+    //         }}
+    //       />
+    //     );
+    //   },
+    // },
   ];
 
   return (
@@ -120,13 +179,26 @@ export default () => {
                     title="Query Params"
                     dataSource={paramsData}
                     setDataSource={setParamsData}
+                    extra={joinUrl}
+                    editableKeys={editableKeys}
+                    setEditableRowKeys={setEditableRowKeys}
                   />
                 ),
               },
               {
                 label: `Headers`,
                 key: '2',
-                children: `这是Headers`,
+                children: (
+                  <EditableTable
+                    columns={paramsColumns}
+                    title="Headers"
+                    dataSource={paramsData}
+                    setDataSource={setParamsData}
+                    extra={joinUrl}
+                    editableKeys={editableKeys}
+                    setEditableRowKeys={setEditableRowKeys}
+                  />
+                ),
               },
               {
                 label: `Body`,
